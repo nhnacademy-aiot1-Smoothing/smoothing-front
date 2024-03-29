7d44636fc31b66bda3608e1846fd3d360fe2ad60
supermicrosoft <30441597+supermicrosoft@users.noreply.github.com>
2024-03-29 15:53:21 +0900

    [Fix]: TestCode 수정

--- src/test/java/live/smoothing/front/interceptor/TokenHeaderInterceptorTest.java
+++ src/test/java/live/smoothing/front/interceptor/TokenHeaderInterceptorTest.java
@@ -13,24 +13,28 @@ import static org.junit.jupiter.api.Assertions.*;
 class TokenHandlerInterceptorTest {
 
     private TokenHandlerInterceptor interceptor;
-
+    private final String accessToken = "smoothing-accessToken";
+    private final String refreshToken = "smoothing-refreshToken";
     @BeforeEach
     void setUp() {
         interceptor = new TokenHandlerInterceptor();
+
     }
 
     @Test
     @DisplayName("쿠키에 토큰이 있으면 헤더에 추가한다.")
     void addTokenHeaderWhenCookiesPresent() throws Exception {
+
+
         MockHttpServletRequest request = new MockHttpServletRequest();
         MockHttpServletResponse response = new MockHttpServletResponse();
 
-        request.setCookies(new Cookie("accessToken", "someAccessToken"), new Cookie("refreshToken", "someRefreshToken"));
+        request.setCookies(new Cookie(accessToken, "someAccessToken"), new Cookie(refreshToken, "someRefreshToken"));
 
         interceptor.preHandle(request, response, null);
 
-        assertEquals("someAccessToken", response.getHeader("accessToken"));
-        assertEquals("someRefreshToken", response.getHeader("refreshToken"));
+        assertEquals("someAccessToken", response.getHeader(accessToken));
+        assertEquals("someRefreshToken", response.getHeader(refreshToken));
     }
 
     @Test
@@ -41,7 +45,7 @@ class TokenHandlerInterceptorTest {
 
         interceptor.preHandle(request, response, null);
 
-        assertNull(response.getHeader("accessToken"));
-        assertNull(response.getHeader("refreshToken"));
+        assertNull(response.getHeader(accessToken));
+        assertNull(response.getHeader(refreshToken));
     }
 }
