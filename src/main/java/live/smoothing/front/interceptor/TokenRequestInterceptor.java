73bcd3b81fdea9b791b6fe9bd3471f5be8b8d85a
nakta-y <nakta.y.biz@gmail.com>
2024-04-01 23:59:57 +0900

    [Refactor] : ThreadLocalToken finally 구문 추가

--- src/main/java/live/smoothing/front/interceptor/TokenRequestInterceptor.java
+++ src/main/java/live/smoothing/front/interceptor/TokenRequestInterceptor.java
@@ -20,21 +20,23 @@ public class TokenRequestInterceptor implements RequestInterceptor {
      * 2. 템플릿에 토큰을 추가한다.
      * 3. ThreadLocalToken에서 토큰을 제거한다.
      *
-     * @param requestTemplate
+     * @param requestTemplate 토큰 헤더를 추가한 요청 템플릿
      */
     @Override
     public void apply(RequestTemplate requestTemplate) {
+        try {
+            TokenWithType token = ThreadLocalToken.TOKEN.get();
 
-        TokenWithType token = ThreadLocalToken.TOKEN.get();
-        if(token != null) {
-            String tokenType = token.getTokenType();
-            String tokenValue = token.getToken();
+            if (token != null) {
+                String tokenType = token.getTokenType();
+                String tokenValue = token.getToken();
 
-            String authorizationToken = tokenType + " " + tokenValue;
+                String authorizationToken = String.format("%s %s", tokenType, tokenValue);
 
-            requestTemplate.header("Authorization", authorizationToken);
+                requestTemplate.header("Authorization", authorizationToken);
+            }
+        } finally {
+            ThreadLocalToken.TOKEN.remove();
         }
-
-        ThreadLocalToken.TOKEN.remove();
     }
 }
