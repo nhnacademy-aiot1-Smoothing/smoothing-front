7a1a0a87f1d63a30b6dec15975be98d57047c247
nakta-y <nakta.y.biz@gmail.com>
2024-04-02 09:06:19 +0900

    [Add] : util jatadoc 추가

--- src/main/java/live/smoothing/front/util/JwtUtil.java
+++ src/main/java/live/smoothing/front/util/JwtUtil.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 
 /**
- *
+ * JWT Token 관련 유틸 클래스
  *
  * @author 박영준, 우혜승
  */
@@ -18,6 +18,13 @@ public class JwtUtil {
 
     private final static ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
+    /**
+     * JWT Token에서 만료시간을 추출하는 메서드
+     *
+     * @param token 문자열 JWT Token
+     * @return 만료시간
+     * @throws JsonProcessingException Json 파싱 중 오류 발생 예외
+     */
     public static Long getExpireTime(String token) throws JsonProcessingException {
         String payload = token.split("\\.")[1];
         String payloadJson = new String(Base64.getDecoder().decode(payload));
@@ -25,6 +32,13 @@ public class JwtUtil {
         return Long.parseLong(jsonNode.get("exp").asText());
     }
 
+    /**
+     * JWT Token에서 권한을 추출하는 메서드
+     *
+     * @param token 문자열 JWT Token
+     * @return 권한 리스트
+     * @throws JsonProcessingException Json 파싱 중 오류 발생 예외
+     */
     public static List<String> getRoles(String token) throws JsonProcessingException {
         String payload = token.split("\\.")[1];
         String payloadJson = new String(Base64.getDecoder().decode(payload));
@@ -33,6 +47,13 @@ public class JwtUtil {
         });
     }
 
+    /**
+     * Jwt Token에서 유저 아이디를 추출하는 메서드
+     *
+     * @param token 문자열 JWT Token
+     * @return 유저 아이디
+     * @throws JsonProcessingException Json 파싱 중 오류 발생 예외
+     */
     public static String getUserId(String token) throws JsonProcessingException {
         String payload = token.split("\\.")[1];
         String payloadJson = new String(Base64.getDecoder().decode(payload));
