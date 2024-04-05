f6ac610fdf45fbc3cda440ce5660a2ca3848787e
doramonz <whs3867@icloud.com>
2024-04-05 09:06:56 +0900

    [Feat] : Logout auth 연동

--- src/main/java/live/smoothing/front/security/CustomLogoutHandler.java
+++ src/main/java/live/smoothing/front/security/CustomLogoutHandler.java
@@ -1,6 +1,9 @@
 package live.smoothing.front.security;
 
+import live.smoothing.front.adapter.AuthAdaptor;
+import live.smoothing.front.dto.RefreshTokenRequest;
 import live.smoothing.front.util.CookieUtil;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 
@@ -8,18 +11,24 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@RequiredArgsConstructor
 public class CustomLogoutHandler implements LogoutHandler {
+    private final AuthAdaptor authAdaptor;
+
     @Override
     public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
         Cookie accessToken = CookieUtil.getCookieByName(request.getCookies(), "smoothing_accessToken");
         Cookie refreshToken = CookieUtil.getCookieByName(request.getCookies(), "smoothing_refreshToken");
-        if(accessToken != null) {
+        if (accessToken != null) {
             accessToken.setMaxAge(0);
             response.addCookie(accessToken);
         }
-        if(refreshToken != null) {
+        if (refreshToken != null) {
             refreshToken.setMaxAge(0);
             response.addCookie(refreshToken);
+        }
+        if (refreshToken != null) {
+            authAdaptor.logout(new RefreshTokenRequest(refreshToken.getValue()));
         }
     }
 }
