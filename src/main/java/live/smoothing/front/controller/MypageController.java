a1a6f935ade21b6130fe44ed0a6dbfc94c5c8220
haaazzi <haa.zzing@gmail.com>
2024-04-28 19:59:29 +0900

    [Feat]: 포인트 잔액/상세내역 조회 view 구현

--- src/main/java/live/smoothing/front/controller/MypageController.java
+++ src/main/java/live/smoothing/front/controller/MypageController.java
@@ -1,14 +1,66 @@
 package live.smoothing.front.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import live.smoothing.front.adapter.UserAdapter;
+import live.smoothing.front.user.dto.UserPointDetailResponse;
+import live.smoothing.front.util.JwtUtil;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
 public class MypageController {
 
+    private final UserAdapter userAdapter;
+
+    public MypageController(UserAdapter userAdapter) {
+
+        this.userAdapter = userAdapter;
+    }
+
     @GetMapping("/mypage")
-    public String mypage() {
+    public String mypage(Model model, HttpServletRequest request) throws JsonProcessingException {
+
+        String jwtToken = null;
+        Cookie[] cookies = request.getCookies();
+        if(cookies != null) {
+            for(Cookie cookie : cookies) {
+                if(cookie.getName().equals("smoothing_accessToken")) {
+                    jwtToken = cookie.getValue();
+                    break;
+                }
+            }
+        }
+        String userId = JwtUtil.getUserId(jwtToken);
+
+        Long pointBalance = userAdapter.getPointBalanceByUserId(userId);
+        model.addAttribute("pointBalance", pointBalance);
 
         return "pages/mypage";
+    }
+
+    @GetMapping("/point-details")
+    public String pointDetail(Model model, HttpServletRequest request) throws JsonProcessingException {
+        String jwtToken = null;
+        Cookie[] cookies = request.getCookies();
+        if(cookies != null) {
+            for(Cookie cookie : cookies) {
+                if(cookie.getName().equals("smoothing_accessToken")) {
+                    jwtToken = cookie.getValue();
+                    break;
+                }
+            }
+        }
+        String userId = JwtUtil.getUserId(jwtToken);
+        List<UserPointDetailResponse> pointDetails = userAdapter.getPointDetailsByUserId(userId);
+        model.addAttribute("pointDetails", pointDetails);
+
+        Long pointBalance = userAdapter.getPointBalanceByUserId(userId);
+        model.addAttribute("pointBalance", pointBalance);
+        return "pages/point";
     }
 }
---
+++ src/main/java/live/smoothing/front/user/dto/UserPointDetailResponse.java
@@ -0,0 +1,20 @@
+package live.smoothing.front.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserPointDetailResponse {
+    private Long pointDetailId;
+    private String userId;
+    private Long pointDetailAmount;
+    private String pointDetailType;
+    private LocalDateTime pointRecordDate;
+}
