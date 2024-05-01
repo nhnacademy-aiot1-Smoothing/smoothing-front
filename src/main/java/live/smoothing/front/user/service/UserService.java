425543a2903d97c9048d3ad5aa1dcd74d343d7e1
Jiyoon <bystar1687@nate.com>
2024-05-01 17:38:07 +0900

    [Feat]: 회원 승인 목록에 필요한 기능들 구현

--- src/main/java/live/smoothing/front/user/service/UserService.java
+++ src/main/java/live/smoothing/front/user/service/UserService.java
@@ -1,8 +1,13 @@
 package live.smoothing.front.user.service;
 
 import live.smoothing.front.auth.dto.email.MessageResponse;
+import live.smoothing.front.user.dto.WaitingUser;
+import live.smoothing.front.user.dto.request.UserApproveRequest;
 import live.smoothing.front.user.dto.request.UserCreateRequest;
+import live.smoothing.front.user.dto.response.RoleResponse;
 import live.smoothing.front.user.dto.response.UserAttendanceResponse;
+
+import java.util.List;
 
 /**
  * 유저 관련 서비스 인터페이스
@@ -21,4 +26,14 @@ public interface UserService {
     UserAttendanceResponse getUserAttendance(int year, int month);
 
     MessageResponse doAttendanceCheck();
+
+    List<WaitingUser> getWaitingUserList();
+
+    List<WaitingUser> getWaitingUserList(int page, int size);
+
+    MessageResponse approveUser(UserApproveRequest request);
+
+    MessageResponse rejectUser(String userId);
+
+    List<RoleResponse> getAllRoles();
 }
--- src/main/java/live/smoothing/front/user/service/impl/UserServiceImpl.java
+++ src/main/java/live/smoothing/front/user/service/impl/UserServiceImpl.java
@@ -3,11 +3,19 @@ package live.smoothing.front.user.service.impl;
 
 import live.smoothing.front.adapter.UserApiAdapter;
 import live.smoothing.front.auth.dto.email.MessageResponse;
+import live.smoothing.front.user.dto.WaitingUser;
+import live.smoothing.front.user.dto.request.UserApproveRequest;
 import live.smoothing.front.user.dto.request.UserCreateRequest;
+import live.smoothing.front.user.dto.response.RoleResponse;
 import live.smoothing.front.user.dto.response.UserAttendanceResponse;
 import live.smoothing.front.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * 유저 서비스 구현체
@@ -39,5 +47,35 @@ public class UserServiceImpl implements UserService {
     public MessageResponse doAttendanceCheck() {
 
         return userApiAdapter.doAttendanceCheck();
+    }
+
+    @Override
+    public List<WaitingUser> getWaitingUserList() {
+
+        return userApiAdapter.getWaitingUserList();
+    }
+
+    @Override
+    public List<WaitingUser> getWaitingUserList(int page, int size) {
+
+        return userApiAdapter.getWaitingUserList(page, size);
+    }
+
+    @Override
+    public MessageResponse approveUser(UserApproveRequest request) {
+
+        return userApiAdapter.approveUser(request);
+    }
+
+    @Override
+    public MessageResponse rejectUser(String userId) {
+
+        return userApiAdapter.rejectUser(userId);
+    }
+
+    @Override
+    public List<RoleResponse> getAllRoles() {
+
+        return userApiAdapter.getAllRoles();
     }
 }
