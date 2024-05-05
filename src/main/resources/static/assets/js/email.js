ce82360b4998d17dbddfd2fc40f0278be8f23f78
Jiyoon <bystar1687@nate.com>
2024-05-05 20:04:46 +0900

    [Feat]: email 인증번호 발급 시 타이머 추가

--- src/main/resources/static/assets/js/email.js
+++ src/main/resources/static/assets/js/email.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
 
     let sendCertificationNumberButton = document.getElementById('sendCertificationNumber');
+    let timerText = document.createElement('span');
+    let timerInterval;
 
     sendCertificationNumberButton.addEventListener('click', function () {
 
@@ -21,14 +23,42 @@ document.addEventListener('DOMContentLoaded', function () {
         xhr.onreadystatechange = function () {
             if (xhr.readyState === XMLHttpRequest.DONE) {
                 if (xhr.status === 200) {
-                    console.log(xhr.responseText);
                     alert("인증번호가 발급되었습니다.");
+                    sendCertificationNumberButton.disabled = true;
+                    timerInterval = startTimer();
                 } else {
                     console.log("오류 발생");
                 }
             }
         };
     });
+
+    function startTimer() {
+
+        let totalTime = 180;
+        let minutes, seconds;
+        timerText.innerText = '3:00';
+        sendCertificationNumberButton.parentElement.appendChild(timerText);
+
+        return setInterval(function () {
+            minutes = Math.floor(totalTime / 60);
+            seconds = totalTime % 60;
+
+            minutes = minutes < 10 ? '0' + minutes : minutes;
+            seconds = seconds < 10 ? '0' + seconds : seconds;
+
+            timerText.innerText = minutes + ':' + seconds;
+
+            if (totalTime <= 0) {
+                clearInterval(timerInterval);
+                timerText.innerText='';
+                sendCertificationNumberButton.disabled = false;
+            } else {
+                totalTime--;
+            }
+        }, 1000);
+
+    }
 
     let verificationNumberButton = document.getElementById('verification');
 
@@ -52,6 +82,8 @@ document.addEventListener('DOMContentLoaded', function () {
             if (xhr.readyState === XMLHttpRequest.DONE) {
                 if (xhr.status === 200) {
                     alert("인증 완료 되었습니다.");
+                    verificationNumberButton.disabled = true;
+                    clearInterval(timerInterval);
                 } else if (xhr.status === 401) {
                     alert("인증번호를 다시 확인해주세요.");
                 } else {
