da2436ca1451577ba4a93b24658022076fa0f398
doramonz <whs3867@gmail.com>
2024-05-07 17:12:06 +0900

    [feature] : Broker

--- src/main/java/live/smoothing/front/controller/BrokerController.java
+++ src/main/java/live/smoothing/front/controller/BrokerController.java
@@ -1,16 +1,26 @@
 package live.smoothing.front.controller;
 
+import live.smoothing.front.device.dto.BrokerListResponse;
+import live.smoothing.front.device.service.BrokerService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class BrokerController {
 
-    @GetMapping("/broker")
-    public String broker(@PageableDefault Pageable pageable) {
+    private final BrokerService brokerService;
 
+    @GetMapping("/broker")
+    public String broker(@PageableDefault Pageable pageable,
+                         Model model) {
+
+        BrokerListResponse brokers = brokerService.getBrokers(pageable);
+        model.addAttribute("brokers", brokers.getBrokers());
         return "pages/broker";
     }
 }
