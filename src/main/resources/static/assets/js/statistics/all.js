92801db1b144228e88f9aad1948ea0960c01a793
haaazzi <haa.zzing@gmail.com>
2024-05-09 10:04:30 +0900

    [Fix]: 최근 일주일 시간대별 전력량 그래프

--- src/main/resources/static/assets/js/statistics/all.js
+++ src/main/resources/static/assets/js/statistics/all.js
@@ -1,6 +1,5 @@
 (async () => {
 
-    // Load the dataset
     const data = await fetch(
         'https://demo-live-data.highcharts.com/aapl-c.json'
     ).then(response => response.json());
@@ -11,14 +10,13 @@
         }
     });
 
-    // Create the chart
     Highcharts.stockChart('container', {
 
         rangeSelector: {
             allButtonsEnabled: true,
             buttons: [{
-                type: 'month',
-                count: 3,
+                type: 'week',
+                count: 1,
                 text: 'Day',
                 dataGrouping: {
                     forced: true,
@@ -58,15 +56,43 @@
             enabled: false
         },
 
-        series: [{
+        plotOptions: {
+            area: {
+                fillColor: {
+                    linearGradient: {
+                        x1: 0,
+                        y1: 0,
+                        x2: 0,
+                        y2: 1
+                    },
+                    stops: [
+                        [0, Highcharts.getOptions().colors[0]],
+                        [1, Highcharts.color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                    ]
+                },
+                marker: {
+                    radius: 2
+                },
+                lineWidth: 1,
+                states: {
+                    hover: {
+                        lineWidth: 1
+                    }
+                },
+                threshold: null
+            }
+        },
+
+    series: [{
             name: 'AAPL',
             data: data,
             marker: {
-                enabled: null, // auto
+                enabled: null,
                 radius: 3,
                 lineWidth: 1,
                 lineColor: '#FFFFFF'
             },
+            type: 'area',
             tooltip: {
                 valueDecimals: 2
             }
