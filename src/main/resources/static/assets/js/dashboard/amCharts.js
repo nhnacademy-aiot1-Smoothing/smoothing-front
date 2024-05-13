ad441551401fe9e90c94bafb876beb9ac99bfd08
nakta-y <nakta.y.biz@gmail.com>
2024-05-13 21:33:30 +0900

    [Feat] : 메인 페이지 태그 선택 구현

--- src/main/resources/static/assets/js/dashboard/amCharts.js
+++ src/main/resources/static/assets/js/dashboard/amCharts.js
@@ -1,129 +1,10 @@
-function createRotatedColumnChart(div) {
+var root;
+function createRaderChart(div, tags) {
+    if (root) {
+        root.dispose();
+    }
 
-    var root = am5.Root.new(div);
-
-// Set themes
-// https://www.amcharts.com/docs/v5/concepts/themes/
-    root.setThemes([
-        am5themes_Animated.new(root)
-    ]);
-
-// Create chart
-// https://www.amcharts.com/docs/v5/charts/xy-chart/
-    var chart = root.container.children.push(
-        am5xy.XYChart.new(root, {
-            panX: true,
-            panY: true,
-            wheelX: "panX",
-            wheelY: "zoomX",
-            paddingLeft: 5,
-            paddingRight:5
-        })
-    );
-
-// Add cursor
-// https://www.amcharts.com/docs/v5/charts/xy-chart/cursor/
-    var cursor = chart.set("cursor", am5xy.XYCursor.new(root, {}));
-    cursor.lineY.set("visible", false);
-
-// Create axes
-// https://www.amcharts.com/docs/v5/charts/xy-chart/axes/
-    var xRenderer = am5xy.AxisRendererX.new(root, {
-        minGridDistance: 60,
-        minorGridEnabled: true
-    });
-
-    var xAxis = chart.xAxes.push(
-        am5xy.CategoryAxis.new(root, {
-            maxDeviation: 0.3,
-            categoryField: "country",
-            renderer: xRenderer,
-            tooltip: am5.Tooltip.new(root, {})
-        })
-    );
-
-    xRenderer.grid.template.setAll({
-        location: 1
-    })
-
-    var yAxis = chart.yAxes.push(
-        am5xy.ValueAxis.new(root, {
-            maxDeviation: 0.3,
-            renderer: am5xy.AxisRendererY.new(root, {
-                strokeOpacity: 0.1
-            })
-        })
-    );
-
-// Create series
-// https://www.amcharts.com/docs/v5/charts/xy-chart/series/
-    var series = chart.series.push(
-        am5xy.ColumnSeries.new(root, {
-            name: "Series 1",
-            xAxis: xAxis,
-            yAxis: yAxis,
-            valueYField: "value",
-            sequencedInterpolation: true,
-            categoryXField: "country"
-        })
-    );
-
-    series.columns.template.setAll({
-        width: am5.percent(120),
-        fillOpacity: 0.9,
-        strokeOpacity: 0
-    });
-    series.columns.template.adapters.add("fill", (fill, target) => {
-        return chart.get("colors").getIndex(series.columns.indexOf(target));
-    });
-
-    series.columns.template.adapters.add("stroke", (stroke, target) => {
-        return chart.get("colors").getIndex(series.columns.indexOf(target));
-    });
-
-    series.columns.template.set("draw", function(display, target) {
-        var w = target.getPrivate("width", 0);
-        var h = target.getPrivate("height", 0);
-        display.moveTo(0, h);
-        display.bezierCurveTo(w / 4, h, w / 4, 0, w / 2, 0);
-        display.bezierCurveTo(w - w / 4, 0, w - w / 4, h, w, h);
-    });
-
-// Set data
-    var data = [{
-        country: "USA",
-        value: 1225
-    }, {
-        country: "China",
-        value: 1882
-    }, {
-        country: "Japan",
-        value: 1809
-    }, {
-        country: "Germany",
-        value: 1322
-    }, {
-        country: "UK",
-        value: 1122
-    }, {
-        country: "France",
-        value: 1114
-    }, {
-        country: "India",
-        value: 984
-    }, ];
-
-    xAxis.data.setAll(data);
-    series.data.setAll(data);
-
-// Make stuff animate on load
-// https://www.amcharts.com/docs/v5/concepts/animations/
-    series.appear(1000);
-    chart.appear(1000, 100);
-}
-
-function createRaderChart(div) {
-    var root = am5.Root.new("chartdiv2");
+    root = am5.Root.new(div);
 
 // Set themes
 // https://www.amcharts.com/docs/v5/concepts/themes/
@@ -195,7 +76,7 @@ function createRaderChart(div) {
     });
 
 // Set data
-    fetch("/sensor/kwh/daily/value?tags=")
+    fetch("/sensor/kwh/daily/value?tags=" + tags)
         .then(response => response.json())
         .then(res => getTop5ValueSensors(res))
         .then(data => {
@@ -210,7 +91,8 @@ function createRaderChart(div) {
         const data = [];
 
         response.data.sort((a, b) => b.value - a.value);
-        for (let i = 0; i < 7; i++) {
+        const max = response.data.length < 5 ? response.data.length : 5;
+        for (let i = 0; i < max; i++) {
             data.push({
                 "sensorName": response.data[i].sensorName,
                 "value": response.data[i].value
@@ -221,7 +103,7 @@ function createRaderChart(div) {
     }
 
     function updateData() {
-        fetch("/sensor/kwh/daily/value?tags=")
+        fetch("/sensor/kwh/daily/value?tags=" + tags)
             .then(response => response.json())
             .then(res => getTop5ValueSensors(res))
             .then(data => {
@@ -308,11 +190,8 @@ function createRaderChart(div) {
         });
     }
 
-
-// Make stuff animate on load
-// https://www.amcharts.com/docs/v5/concepts/animations/
     series.appear(1000);
     chart.appear(1000, 100);
 }
 
-createRaderChart("chartdiv2");
+createRaderChart("chartdiv2", "");
