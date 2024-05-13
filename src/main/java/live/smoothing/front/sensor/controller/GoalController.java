08d1700dfb3fe1f6534e4df493d9b469433819c8
haaazzi <haa.zzing@gmail.com>
2024-05-13 09:27:16 +0900

    [Feat]: 연도별 월단위 전력량 그래프(목표량 함께 표시)

--- src/main/java/live/smoothing/front/sensor/controller/GoalController.java
+++ src/main/java/live/smoothing/front/sensor/controller/GoalController.java
@@ -1,10 +1,14 @@
 package live.smoothing.front.sensor.controller;
 
 import live.smoothing.front.sensor.dto.KwhGoalResponse;
+import live.smoothing.front.sensor.dto.MonthlyGoalResponse;
 import live.smoothing.front.sensor.service.SensorService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,6 +18,13 @@ public class GoalController {
 
     @GetMapping("/sensor/goal/current")
     public KwhGoalResponse getGoal() {
+
         return sensorService.getKwhGoal();
+    }
+
+    @GetMapping("/sensor/goals/history")
+    public List<MonthlyGoalResponse> getMonthlyGoals(@RequestParam String year) {
+
+        return sensorService.getMonthlyGoals(year);
     }
 }
