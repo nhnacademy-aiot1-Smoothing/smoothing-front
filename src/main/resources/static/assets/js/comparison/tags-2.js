3c68fd9682840eca6cf4597f96585abbc6aadb94
haaazzi <haa.zzing@gmail.com>
2024-05-14 17:32:57 +0900

    [Feat]: 날짜 선택 / Tag 선택 비교

--- src/main/resources/static/assets/js/comparison/tags-2.js
+++ src/main/resources/static/assets/js/comparison/tags-2.js
@@ -1,10 +1,10 @@
-const tagCheckboxGroup = document.getElementById("tagCheckboxGroup");
+const tagCheckboxGroup2 = document.getElementById("tagCheckboxGroup2");
 
 fetch('/api/device/tags')
     .then(response => response.json())
     .then(data => {
         const tags = data.tags;
-        console.log(tags)
+
         tags.forEach(tag => {
             const checkbox = document.createElement("input");
             checkbox.type = "checkbox";
@@ -15,13 +15,9 @@ fetch('/api/device/tags')
             label.appendChild(document.createTextNode(tag.tagName));
             label.appendChild(checkbox);
 
-            tagCheckboxGroup.appendChild(label);
+            tagCheckboxGroup2.appendChild(label);
         });
     })
     .catch(error => {
         console.error('태그 목록을 가져오는 중 오류 발생:', error);
     });
-
-document.getElementById("tagSelectButton").addEventListener("click", function() {
-    const selectedTags = Array.from(document.querySelectorAll("input[name=tag]:checked")).map(checkbox => checkbox.value);
-});
