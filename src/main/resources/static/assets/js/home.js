65acff3abe5217aaee325ffbf959653ce5a48ab5
haaazzi <haa.zzing@gmail.com>
2024-05-26 23:36:13 +0900

    [Fix]: fcm 오류 해결

--- src/main/resources/static/assets/js/home.js
+++ src/main/resources/static/assets/js/home.js
@@ -1,11 +1,10 @@
 let firebaseConfig = {
-    apiKey: "AIzaSyBYjQ_3ClLSfoS7pQLJMfhJSAHbmoNlUJ0",
-    authDomain: "smoothing-4e445.firebaseapp.com",
-    projectId: "smoothing-4e445",
-    storageBucket: "smoothing-4e445.appspot.com",
-    messagingSenderId: "1008788884422",
-    appId: "1:1008788884422:web:b003901891b30682cf00c7",
-    measurementId: "G-WSZGQMJW1N"
+        apiKey: "AIzaSyASZ2bBr3yDAkbR5JmUtRskDU8anEFxhFI",
+        authDomain: "smoothing-test2.firebaseapp.com",
+        projectId: "smoothing-test2",
+        storageBucket: "smoothing-test2.appspot.com",
+        messagingSenderId: "265747573760",
+        appId: "1:265747573760:web:a9bb536c87896e3c29fce5"
 };
 
 firebase.initializeApp(firebaseConfig);
