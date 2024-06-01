7bcfad118b3422c60b2af688d80be69c840e9387
Jiyoon <bystar1687@nate.com>
2024-06-01 21:16:01 +0900

    [Design]: 페이지 디자인 수정

--- src/main/resources/static/assets/js/login.js
+++ src/main/resources/static/assets/js/login.js
@@ -1,3 +1,25 @@
+const inputs = document.querySelectorAll(".input");
+
+
+function addcl(){
+    let parent = this.parentNode.parentNode;
+    parent.classList.add("focus");
+}
+
+function remcl(){
+    let parent = this.parentNode.parentNode;
+    if(this.value == ""){
+        parent.classList.remove("focus");
+    }
+}
+
+
+inputs.forEach(input => {
+    input.addEventListener("focus", addcl);
+    input.addEventListener("blur", remcl);
+});
+
+//Source :- https://github.com/sefyudem/Responsive-Login-Form/blob/master/img/avatar.svg
 document.addEventListener('DOMContentLoaded', function () {
 
     let loginButton = document.getElementById('loginButton');
