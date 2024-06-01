c0ce72128094e89164ad2aa351ca5088bfc63c77
Maru375 <www980304@gmail.com>
2024-06-01 22:12:05 +0900

    [Feat]: 전력 사용량 예측 그래프 구현

--- src/main/resources/static/assets/js/ai/ai_predictional.js
+++ src/main/resources/static/assets/js/ai/ai_predictional.js
@@ -2,33 +2,33 @@ Highcharts.chart('prediction-graph', {
     chart: {
         type: 'line',
         events: {
-                load: function() {
-                    const chart = this;
-                    fetch("/ai/power-usage/prediction?measurement=power_usage&field=socket_power")
-                        .then(response => response.json())
-                        .then(res => {
+            load: function () {
+                const chart = this;
+                fetch("/ai/power-usage/prediction?measurement=power_usage&field=socket_power")
+                    .then(response => response.json())
+                    .then(res => {
                             const data = [];
                             for (let i = 0; i < res.length; i++) {
                                 data.push([new Date(res[i].time), res[i].value]);
                             }
 
                             chart.series[0].setData(data, true, true, true);
-                            }
-                        )
-                        .then(fetch("/ai/power-usage/actual?location=class_a_floor_heating&description=w")
+                        }
+                    )
+                    .then(fetch("/ai/power-usage/actual?location=class_a_floor_heating&description=w")
                         .then(response => response.json())
                         .then(res => {
-                            const data = [];
-                            for (let i = 0; i < res.length; i++) {
-                                data.push([new Date(res[i].time), res[i].value]);
-                            }
+                                const data = [];
+                                for (let i = 0; i < res.length; i++) {
+                                    data.push([new Date(res[i].time), res[i].value]);
+                                }
 
-                            console.log(data);
+                                console.log(data);
 
-                            chart.series[1].setData(data, true, true, true);
+                                chart.series[1].setData(data, true, true, true);
                             }
                         ))
-                }
+            }
         }
     },
     title: {
