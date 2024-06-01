8e50fca1136eca7dccbc95b0a3f57b5cde8e73d5
haaazzi <haa.zzing@gmail.com>
2024-06-02 01:27:36 +0900

    [Fix]: Phase DTO 추가

---
+++ src/main/java/live/smoothing/front/sensor/dto/Phase.java
@@ -0,0 +1,14 @@
+package live.smoothing.front.sensor.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Phase {
+
+    private String time;
+    private double value;
+}
