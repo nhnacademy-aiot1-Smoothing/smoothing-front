a4344ad77ed02ed42a3374c04e0d635be9b85986
Jiyoon <bystar1687@nate.com>
2024-06-02 21:41:09 +0900

    [Design]: 페이지 디자인 수정

--- src/main/resources/static/assets/js/comparison/tags-2.js
+++ src/main/resources/static/assets/js/comparison/tags-2.js
@@ -1,22 +1,61 @@
 const tagCheckboxGroup2 = document.getElementById("tagCheckboxGroup2");
 
+// fetch('/api/device/tags')
+//     .then(response => response.json())
+//     .then(data => {
+//         const tags = data.tags;
+//
+//         tags.forEach(tag => {
+//             const checkbox = document.createElement("input");
+//             checkbox.type = "checkbox";
+//             checkbox.name = "tag";
+//             checkbox.value = tag.tagName;
+//
+//             const label = document.createElement("label");
+//             label.appendChild(document.createTextNode(tag.tagName));
+//             label.appendChild(checkbox);
+//
+//             tagCheckboxGroup2.appendChild(label);
+//         });
+//     })
+//     .catch(error => {
+//         console.error('태그 목록을 가져오는 중 오류 발생:', error);
+//     });
+
 fetch('/api/device/tags')
     .then(response => response.json())
     .then(data => {
         const tags = data.tags;
 
+        const div = document.createElement("div");
+        div.classList.add("list-container");
+        div.style.maxHeight = "200px";
+        div.style.overflowY = "auto";
+
+        const ul = document.createElement("ul");
+        ul.classList.add("list-group");
+
         tags.forEach(tag => {
+            const li = document.createElement("li");
+            li.classList.add("list-group-item");
+
             const checkbox = document.createElement("input");
             checkbox.type = "checkbox";
             checkbox.name = "tag";
             checkbox.value = tag.tagName;
+            checkbox.classList.add("form-check-input", "me-1");
 
             const label = document.createElement("label");
-            label.appendChild(document.createTextNode(tag.tagName));
+            label.classList.add("form-check-label");
             label.appendChild(checkbox);
+            label.appendChild(document.createTextNode(tag.tagName));
 
-            tagCheckboxGroup2.appendChild(label);
+            li.appendChild(label);
+            ul.appendChild(li);
         });
+
+        div.appendChild(ul);
+        tagCheckboxGroup2.appendChild(div);
     })
     .catch(error => {
         console.error('태그 목록을 가져오는 중 오류 발생:', error);
