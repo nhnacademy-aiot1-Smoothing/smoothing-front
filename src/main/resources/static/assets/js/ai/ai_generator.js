119551e15d495bf3a7745749806978160a65ce9f
Maru375 <www980304@gmail.com>
2024-06-03 14:05:23 +0900

    [Refactor]: Ai 서비스 페이지 수정

--- src/main/resources/static/assets/js/ai/ai_generator.js
+++ src/main/resources/static/assets/js/ai/ai_generator.js
@@ -86,11 +86,20 @@ function create1DPowerGeneration() {
             enabled: false
         },
         series: [{
-            color: 'rgba(158, 159, 163, 0.5)',
-            pointPlacement: -0.2,
-            linkedTo: 'main',
-            data: [],
-            name: '주간 발전량'
+            name: '발전량',
+            id: 'main',
+            dataSorting: {
+                matchByName: true
+            },
+            dataLabels: [{
+                enabled: false,
+                inside: true,
+                style: {
+                    fontSize: '16px',
+                    color: '#FFF'
+                }
+            }],
+            data: []
         }]
     });
 }
